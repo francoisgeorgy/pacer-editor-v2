51037d61275b543cfd94b998c6d6149e5ffef0d6
François Georgy <francois.georgy@gmail.com>
2021-10-05 15:54:54 +0200

    UI improvement

--- src/components/MenuButtons.jsx
+++ src/components/MenuButtons.jsx
@@ -1,33 +1,27 @@
-import React, {Component, useRef} from "react";
+import React, {useRef} from "react";
 import {observer} from "mobx-react-lite";
 import {stores} from "../stores";
 import FACTORY_PRESETS from "../data/factory-presets.json";
 import {mergeDeep, parseSysexDump} from "../pacer/sysex";
 
-/* copied from ActionButtons */
-
 export const MenuButtons = observer(() => {
 
     const inputOpenFileRef = useRef(null);
+
+    function onChangeFile(e) {
+        let file = e.target.files[0];
+        if (file) {
+            stores.state.readFiles([file]).then(() => console.log("done reading files"));;
+        }
+    }
 
     function onInputFile(e) {
         inputOpenFileRef.current.click()
     }
 
-    function onChangeFile(e) {
-        let file = e.target.files[0];
-        // noinspection JSIgnoredPromiseFromCall
-        if (file) {
-            stores.state.readFiles([file]);
-        }
-    }
-
     function loadFactoryDefaults() {
-        // console.log("FACTORY_PRESETS", typeof FACTORY_PRESETS, FACTORY_PRESETS);
         const data = Uint8Array.from(Object.values(FACTORY_PRESETS));
-        // console.log("data", data);
         stores.state.data = mergeDeep(parseSysexDump(data));
-        // stores.state.storeBytes(data);
     }
 
     function clearData() {
@@ -46,49 +40,33 @@ export const MenuButtons = observer(() => {
     };
 */
 
-    // render() {
-        // const output = stores.state.midi.output;
-        // const data = stores.state.data;
+    const canRead = stores.midi.deviceConnected;
+    const canWrite = stores.midi.deviceConnected && stores.state.changed;
 
-        // console.log("connected", stores.state.connected);
+    return (
+        <div className="menu-buttons">
 
-        const canRead = stores.midi.deviceConnected;
-        const canWrite = stores.midi.deviceConnected && stores.state.changed;
+            <button className={`action-button read ${canRead ? "" : "disabled"}`}
+                    disabled={canRead ? "" : true}
+                    onClick={() => stores.midi.readFullDump()}
+                    title="Read all presets from the Pacer">Read Pacer</button>
 
-        return (
-            <div className="menu-buttons">
+            <button className={`action-button update ${canWrite ? "" : "disabled"}`}
+                    disabled={canWrite ? "" : true}
+                    onClick={() => stores.state.updatePacer()}
+                    title="Save the changes in the Pacer">Update Pacer</button>
 
-                <button className={`action-button read ${canRead ? "" : "disabled"}`}
-                        disabled={canRead ? "" : true}
-                        onClick={() => stores.midi.readFullDump()}
-                        title="Read all presets from the Pacer">Read Pacer</button>
+            <input ref={inputOpenFileRef} type="file" style={{display:"none"}} onChange={onChangeFile} />
+            <button className="action-button" onClick={onInputFile}
+                    title="Import the presets configuration with a SysEx file">Load sysex file</button>
 
-                <button className={`action-button update ${canWrite ? "" : "disabled"}`}
-                        disabled={canWrite ? "" : true}
-                        onClick={() => stores.state.updatePacer()}
-                        title="Save the changes in the Pacer">Update Pacer</button>
+            <button className="action-button" onClick={loadFactoryDefaults}
+                    title="Load the Factory Preset">Load Factory</button>
 
-                <input ref={inputOpenFileRef} type="file" style={{display:"none"}} onChange={onChangeFile} />
-                <button className="action-button" onClick={onInputFile}
-                        title="Import the presets configuration with a SysEx file">Load sysex file</button>
+            <button className="action-button" onClick={clearData}
+                    title="Clear the editor memory">Reset editor</button>
 
-                <button className="action-button" onClick={loadFactoryDefaults}
-                        title="Load the Factory Preset">Load Factory</button>
+        </div>
+    );
 
-                <button className="action-button" onClick={clearData}
-                        title="Clear the editor memory">Reset editor</button>
-
-            </div>
-        );
-    // }
 });
-
-/*
-    <div className="preset-buttons col align-col-bottom">
-        <div>Click any preset to load only this preset.</div>
-        {data && <button onClick={toggleExtControls}>{stores.state.extControls ? "Hide external controls" : "Show external controls"}</button>}
-        {data && <button onClick={toggleBase}>{stores.state.decBase ? "Display numbers in hex" : "Display numbers in dec"}</button>}
-    </div>
-*/
-
-// export default inject('state')(observer(MenuButtons));
--- src/pages/ImportExport.jsx
+++ src/pages/ImportExport.jsx
@@ -1,20 +1,20 @@
-import React, {useRef, useState} from 'react';
+import React, {useRef} from 'react';
 import {stores} from "../stores";
 import {observer} from "mobx-react-lite";
 import {getFullNonGlobalConfigSysex} from "../pacer/sysex";
-import {DownloadHex} from "../components/DownloadHex";
 import {PresetSelectorAndButtons} from "../components/PresetSelectorAndButtons";
 import {DownloadBin} from "../components/DownloadBin";
 import "./ImportExport.css";
 
 export const ImportExport = observer(() => {
 
-    // const [status, setStatus] = useState(null);
     const inputOpenFileRef = useRef(null);
 
     function onChangeFile(e) {
         let file = e.target.files[0];
-        stores.state.readFiles([file]);
+        if (file) {
+            stores.state.readFiles([file]).then(() => console.log("done reading files"));
+        }
     }
 
     function onInputFile(e) {
@@ -22,107 +22,72 @@ export const ImportExport = observer(() => {
     }
 
     function sendToPacer() {
-        stores.midi.sendToPacer();
+        stores.midi.sendToPacer().then(r => () => console.log("done sending sysex"));
     }
 
     const data = stores.state.data;
 
     return (
-        <div className="content">
+        <div className="content import-export">
 
-            <div className="xmb-20">
-                <PresetSelectorAndButtons showClearButton={true} overview={true}
-                                          title="Import/Export presets"
-                                          subtitle="Select the presets to export or send:" />
-            </div>
+            <PresetSelectorAndButtons showClearButton={true} overview={true}
+                                      title="Import/Export presets"
+                                      subtitle="Select the presets to export or send:" />
 
             <div className="content-row-content first dump-wrapper">
 
-{/*
-                <div className="">
-                    <p>
-                        The Global Config is not included.
-                    </p>
-                </div>
-*/}
-
-                <div className="row">
+                <div className="import-export-actions">
                     <div>
-                    <div className="xmt-20">
-                        <h3 className="mt-10">Export to file:</h3>
-                    </div>
-                    <div className="mt-10">
-                        {/*<DownloadAllPresets />*/}
-                        <DownloadBin data={() => getFullNonGlobalConfigSysex(data, true, true)} filename={`pacer-patch`} addTimestamp={true} label="Export"/>
-                        <span className="light">{stores.state.allSelected() ? 'all presets' : stores.state.noneSelected() ? '' : `presets ${stores.state.getOverviewSelectionInfo()}`}</span>
-                        {/*<DownloadBin data={() => getFullNonGlobalConfigSysex(data, false, true)} filename={`pacer-patch`} addTimestamp={true} label="Export all"/>*/}
-                        {/*<DownloadHex data={() => getFullNonGlobalConfigSysex(data, true, true)} filename={`pacer-patch`} addTimestamp={true} label="Export HEX (debug)"/>*/}
-                        {/*<BusyIndicator className="space-left inline-busy" busyMessage={"reading pacer:"} />*/}
-{/*
                         <div>
-                            <p>You will get a file containing sysex messages, in binary format.</p>
-                            <p>This file can be use with this editor but also with any application able to send SysEx data from a file,
-                                like <a href="https://www.snoize.com/sysexlibrarian/" target="_blank" rel="noopener">Sysex Librarian</a> or <a href="https://www.bome.com/products/sendsx" target="_blank" rel="noopener">Send SX</a>.</p>
+                            <div className="action-name">Export to file:</div>
                         </div>
-*/}
-                    </div>
+                        <div className="mt-10">
+                            <DownloadBin data={() => getFullNonGlobalConfigSysex(data, true, true)} filename={`pacer-patch`} addTimestamp={true} label="Export"/>
+                            {/*<span className="light">{stores.state.allSelected() ? 'all presets' : stores.state.noneSelected() ? '' : `presets ${stores.state.getOverviewSelectionInfo()}`}</span>*/}
+                            {/*<DownloadBin data={() => getFullNonGlobalConfigSysex(data, false, true)} filename={`pacer-patch`} addTimestamp={true} label="Export all"/>*/}
+                            {/*<DownloadHex data={() => getFullNonGlobalConfigSysex(data, true, true)} filename={`pacer-patch`} addTimestamp={true} label="Export HEX (debug)"/>*/}
+                            {/*<BusyIndicator className="space-left inline-busy" busyMessage={"reading pacer:"} />*/}
+                        </div>
                     </div>
 
-                    <div className="ml-100">
-                    <div className="xmt-20">
-                        <h3 className="mt-10">Import from file:</h3>
-                    </div>
-                    <div className="mt-10">
-                        <input ref={inputOpenFileRef} type="file" style={{display:"none"}} onChange={onChangeFile} />
-                        <div className="row align-center">
-                            <button className="action-button" onClick={onInputFile}>Load file</button>
-                            {/*<Switch onChange={(checked) => console.log(checked)} checked={false} width={48} height={20} className="mr-10 align-self-center" /> Clear the editor's data before importing.*/}
-                        </div>
-{/*
+                    <div className="">
                         <div>
-                            <p>The file must contain sysex messages, in binary format.</p>
-                            <p>You can create such a file with this editor but also with any application able
-                                to save SysEx data into a file,
-                                like <a href="https://www.snoize.com/sysexlibrarian/" target="_blank" rel="noopener">Sysex Librarian</a> or <a href="https://www.bome.com/products/sendsx" target="_blank" rel="noopener">Send SX</a>.</p>
+                            <div className="action-name">Import from file:</div>
                         </div>
-*/}
-                    </div>
+                        <div className="mt-10">
+                            <input ref={inputOpenFileRef} type="file" style={{display:"none"}} onChange={onChangeFile} />
+                            <div className="row align-center">
+                                <button className="action-button" onClick={onInputFile}>Load file</button>
+                                {/*<Switch onChange={(checked) => console.log(checked)} checked={false} width={48} height={20} className="mr-10 align-self-center" /> Clear the editor's data before importing.*/}
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="ml-100">
-
-                        <div className="xmt-20">
-                            <h3 className="mt-10">Send to Pacer<sup>(2)</sup>:</h3>
+                    <div className="">
+                        <div>
+                            <div className="action-name">Send to Pacer:</div>
                         </div>
                         <div className="mt-10">
                             <div className="row align-center">
                                 <button className={`action-button ${stores.midi.deviceConnected ? "" : "disabled"}`} onClick={sendToPacer}>Send</button>
-                                <span className="light">{stores.state.allSelected() ? 'all presets' : stores.state.noneSelected() ? '' : `presets ${stores.state.getOverviewSelectionInfo()}`}</span>
-                                {stores.state.sendProgress && <span>{stores.state.sendProgress}</span>}
+                                {/*<span className="light">{stores.state.allSelected() ? 'all presets' : stores.state.noneSelected() ? '' : `presets ${stores.state.getOverviewSelectionInfo()}`}</span>*/}
+                                {/*{stores.state.sendProgress && <span>{stores.state.sendProgress}</span>}*/}
                             </div>
-{/*
-                            <div>
-                                <p>This will send the selected presets to the Pacer.</p>
-                            </div>
-*/}
                         </div>
-
                     </div>
 
+                    <div className="no-border">
+                        {stores.state.sendProgress}
+                    </div>
                 </div>
 
                 <div>
-                    <h4>Please note:</h4>
                     <ul>
                         <li>The Global Config is not included.</li>
-                        <li>The file contains sysex data in binary format.</li>
-                        <li>This file can be use with this editor but also with any application able to send SysEx data from a file,
-                            like <a href="https://www.snoize.com/sysexlibrarian/" target="_blank" rel="noopener">Sysex Librarian</a> or <a href="https://www.bome.com/products/sendsx" target="_blank" rel="noopener">Send SX</a>.</li>
+                        <li>Only the selected presets are exported or sent.</li>
+                        <li>The file contains sysex data in binary format. This format is compatible with tools like <a href="https://www.snoize.com/sysexlibrarian/" target="_blank" rel="noopener">Sysex Librarian</a> or <a href="https://www.bome.com/products/sendsx" target="_blank" rel="noopener">Send SX</a>.</li>
                     </ul>
-
-                    <p></p>
                 </div>
-
 
                 {/* status &&
                 <div className={`status ${status.severity}`}>
