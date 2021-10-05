773257b24426a6cc3143cdf7db42787c12bcaafd
François Georgy <francois.georgy@gmail.com>
2021-10-05 22:06:14 +0200

    PWA OK

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,20 @@ import './index.css';       // first position because it contains un @charset di
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {App} from './App'
+import { registerSW } from 'virtual:pwa-register'
+
+function noop() {}
+if (import.meta.env.PROD) {
+    console.log = noop;
+    console.warn = noop;
+    console.error = noop;
+}
+
+const updateSW = registerSW({
+    onOfflineReady() {
+        console.log("READY FOR OFFLINE USAGE");
+    },
+});
 
 ReactDOM.render(
     <React.StrictMode>
